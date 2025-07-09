53740975800cd4736ef31b2ca07750bc1ddbe335
Yacine Ould <yacineouldabdel@gmail.com>
2025-07-09 11:48:49 +0200

    MAJ de create_db pour mettre la bonne adresse du dossier api

--- create_db.py
+++ create_db.py
@@ -1,6 +1,11 @@
-from database.db import engine
-from database import models
+import sys
+import os
 
-# Création des tables
-models.Base.metadata.create_all(bind=engine)
+# Ajout du dossier 'api' au path pour permettre les imports
+sys.path.append(os.path.join(os.path.dirname(__file__), 'api'))
+
+from db import engine
+from models import Base
+
+Base.metadata.create_all(bind=engine)
 print("✅ Base de données initialisée.")
