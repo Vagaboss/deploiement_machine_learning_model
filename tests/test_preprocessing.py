31ae993e82e3509b373ffa3b949ea3083e77c00f
Yacine Ould <yacineouldabdel@gmail.com>
2025-07-11 10:36:38 +0200

    Ajout de 2 tests unitaires : test si variable critique manquante et test sur valeur energique négatives

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,5 +1,5 @@
 # tests/test_preprocessing.py
-
+import pytest
 import pandas as pd
 from preprocessing import preprocess_data
 
@@ -60,3 +60,71 @@ def test_preprocess_data_output_shapes():
     assert isinstance(X_train, pd.DataFrame)
     assert isinstance(y_train, pd.Series)
     assert X_train.shape[0] == y_train.shape[0]
+
+
+
+def test_missing_column_building_type():
+    df = pd.DataFrame({
+        'SiteEnergyUse(kBtu)': [10000],
+        'PropertyGFATotal': [2000],
+        # 'BuildingType' est volontairement absent
+    })
+
+    with pytest.raises(KeyError):
+        preprocess_data(df)
+
+
+def test_filter_negative_energy():
+    df = pd.DataFrame({
+        'BuildingType': ['NonResidential']*3,
+        'Comments': [None]*3,
+        'YearsENERGYSTARCertified': [None]*3,
+        'SiteEnergyUse(kBtu)': [-1000, 0, 2000],
+        'OSEBuildingID': [1, 2, 3],
+        'PropertyGFATotal': [1000, 2000, 3000],
+        'Outlier': [None]*3,
+        'NumberofBuildings': [1]*3,
+        'NumberofFloors': [1]*3,
+        'ComplianceStatus': ['Compliant']*3,
+        'ListOfAllPropertyUseTypes': ['Office']*3,
+        'PrimaryPropertyType': ['Office']*3,
+        'Neighborhood': ['A']*3,
+        'YearBuilt': [2000]*3,
+        'Electricity(kWh)': [100]*3,
+        'NaturalGas(kBtu)': [100]*3,
+        'SteamUse(kBtu)': [0]*3,
+        'PropertyGFAParking': [0]*3,
+        'DataYear': [2015]*3,
+        'City': ['Seattle']*3,
+        'State': ['WA']*3,
+        'DefaultData': [False]*3,
+        'ZipCode': [98101]*3,
+        'CouncilDistrictCode': [1]*3,
+        'Latitude': [47.6]*3,
+        'Longitude': [-122.3]*3,
+        'PropertyGFABuilding(s)': [0]*3,
+        'LargestPropertyUseType': ['Office']*3,
+        'SecondLargestPropertyUseType': [None]*3,
+        'SecondLargestPropertyUseTypeGFA': [None]*3,
+        'ThirdLargestPropertyUseType': [None]*3,
+        'ThirdLargestPropertyUseTypeGFA': [None]*3,
+        'ENERGYSTARScore': [50]*3,
+        'NaturalGas(therms)': [100]*3,
+        'GHGEmissionsIntensity': [10]*3,
+        'SiteEUI(kBtu/sf)': [10]*3,
+        'SiteEUIWN(kBtu/sf)': [10]*3,
+        'SourceEUI(kBtu/sf)': [10]*3,
+        'LargestPropertyUseTypeGFA': [100]*3,
+        'Electricity(kBtu)': [1000]*3,
+        'SiteEnergyUseWN(kBtu)': [9000]*3,
+        'TotalGHGEmissions': [100]*3,
+        'PropertyName': ['B1', 'B2', 'B3'],
+        'Address': ['Addr']*3,
+        'TaxParcelIdentificationNumber': ['TP']*3,
+        'SourceEUIWN(kBtu/sf)': [10]*3
+    })
+
+    X_train, X_test, y_train, y_test = preprocess_data(df)
+
+    assert all(y_train > 0)
+    assert all(y_test > 0)
