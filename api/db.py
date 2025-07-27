147a635b1682b46926730fc13afd8c19112d585a
Yacine Ould <yacineouldabdel@gmail.com>
2025-07-27 16:40:21 +0200

    MAJ de db.py

--- api/db.py
+++ api/db.py
@@ -1,12 +1,18 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
-# Connexion à ta base PostgreSQL locale
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:narutolekake1@localhost:5432/build-seatle"
+# Charger les variables d'environnement du fichier .env
+load_dotenv()
 
+# Lire l'URL de la base depuis le fichier .env
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
+# Création de l'engine et de la session
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 
-# Classe de base pour définir les tables
+# Classe de base pour les modèles SQLAlchemy
 Base = declarative_base()
+
